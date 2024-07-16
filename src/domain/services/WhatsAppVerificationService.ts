5068b8206d2ffb071bc732793307b07807f6e186
João Pedro <jpedro1580@gmail.com>
2024-07-16 12:29:16 -0300

    doc: document whatsapp verification service

--- src/domain/services/WhatsAppVerificationService.ts
+++ src/domain/services/WhatsAppVerificationService.ts
@@ -1,5 +1,12 @@
 export type UnsubscribeCallback = () => void;
 
+/**
+ * An interface that defines the behavior of an WhatsApp verification service.
+ * This service is responsible for checking if a phone number is on WhatsApp.
+ * It provides this by using verification connections. These sessions are
+ * responsible for interacting with the WhatsApp servers and providing the status
+ * of a given phone number.
+ */
 export abstract class WhatsAppVerificationService {
   abstract verifyNumber(number: string, sessionId: string): Promise<boolean>;
   abstract createConnection(sessionId: string): Promise<UnsubscribeCallback>;
